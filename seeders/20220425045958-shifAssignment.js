2c51867e954e9093da41c02d6ed675c7808b2b8f
John Agudelo <john.agudelo@osgroup.co>
2022-04-25 00:58:25 -0500

    add more data for tests

--- seeders/20220425045958-shifAssignment.js
+++ seeders/20220425045958-shifAssignment.js
@@ -9,6 +9,30 @@ module.exports = {
       end_date: new Date('2011-01-01 12:00'),
       createdAt: new Date(),
       updatedAt: new Date(),
+    },
+    {
+      HospitalId: 1,
+      NurseId: 2,
+      start_date: new Date('2020-02-01 08:00'),
+      end_date: new Date('2011-02-01 18:00'),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      HospitalId: 2,
+      NurseId: 2,
+      start_date: new Date('2020-01-02 08:00'),
+      end_date: new Date('2011-01-02 12:00'),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      HospitalId: 3,
+      NurseId: 3,
+      start_date: new Date('2020-03-01 08:00'),
+      end_date: new Date('2011-03-01 12:00'),
+      createdAt: new Date(),
+      updatedAt: new Date(),
     }], {});
   },
 
